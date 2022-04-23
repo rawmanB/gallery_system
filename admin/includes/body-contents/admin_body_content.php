ab3a76b61a52e2629540659830f880974c6d40d4
Raman Bhadel <raw.man.353@gmail.com>
2022-04-23 19:14:27 +1000

    db query check

--- admin/includes/body-contents/admin_body_content.php
+++ admin/includes/body-contents/admin_body_content.php
@@ -7,15 +7,20 @@
             <div class="col-lg-12">
                 <h1 class="page-header">
                     Admin
-                    <small><?php
-                            if ($gallery_database->startConncetion()) {
-                                echo  'true';
-                            } else {
-                                echo 'false';
-                            }
-                            ?>
+                    <small>
+                        Dashboard
                     </small>
                 </h1>
+
+                <?php
+                $query = "SELECT * FROM users WHERE user_id= 1";
+
+                $output = $gallery_database->runQuery($query);
+
+                $result = mysqli_fetch_array($output);
+
+                print_r($result);
+                ?>
                 <ol class="breadcrumb">
                     <li>
                         <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
--- admin/includes/db.php
+++ admin/includes/db.php
@@ -10,7 +10,9 @@ class Gallery_Databse
         $this->startConnection();
     }
 
-
+    /**
+     * Start database connection
+     */
     public function startConnection()
     {
         $this->connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -18,6 +20,36 @@ class Gallery_Databse
         if (mysqli_connect_errno()) {
             die('Database conncetion failed' . mysqli_connect_error());
         }
+    }
+    /**
+     * run query function
+     */
+    public function runQuery($query)
+    {
+        $output = mysqli_query($this->connect, $query);
+
+        return $output;
+    }
+
+
+    /**
+     *Confirm query output 
+     */
+
+    private function queryConfirm($output)
+    {
+        if (!$output) {
+            die('Query Failed');
+        }
+    }
+
+    /**
+     * Escape string before db query
+     */
+
+    public function escapeString($string)
+    {
+        return mysqli_real_escape_string($this->connect, $string);
     }
 }
 
