6bce85cd668defce14445f561866b74a4928482d
Raman Bhadel <raw.man.353@gmail.com>
2022-04-23 10:07:31 +1000

    indent

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <?php include("includes/header.php"); ?>
 
 
-        <div class="row">
+<div class="row">
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
-
-    
-            
-          
-         
-
-            </div>
+    <!-- Blog Entries Column -->
+    <div class="col-md-8">
 
 
 
 
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
 
-            
-                 <?php include("includes/sidebar.php"); ?>
+
+    </div>
 
 
 
-        </div>
-        <!-- /.row -->
 
-        <?php include("includes/footer.php"); ?>
+    <!-- Blog Sidebar Widgets Column -->
+    <div class="col-md-4">
+
+
+        <?php include("includes/sidebar.php"); ?>
+
+
+
+    </div>
+    <!-- /.row -->
+
+    <?php include("includes/footer.php"); ?>
